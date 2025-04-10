1566624444053e2c687c22936e3463cd523b3dcb
Mike <007mrice@gmail.com>
2025-04-10 14:53:12 -0500

    Remove nested Git repo from tracking

---
+++ index.js
@@ -0,0 +1,8 @@
+let animals = ["dog", "cat", "fish"];
+let animalSounds = ["bark", "meow", "blub"];
+
+for (let i = 0; i <= animals.length -1; i++) {
+     
+        console.log(animals[i] + " goes " + animalSounds[i]);
+    
+}
